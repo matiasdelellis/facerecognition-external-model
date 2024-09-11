6d73005f288bfab75bd74a53a5a2974bd22195b6
doppelgrau <johannes+github@formann.de>
2024-09-11 15:12:24 +0200

    Create gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,14 @@
+import multiprocessing
+import os
+from distutils.util import strtobool
+
+bind = f"[::]:{os.getenv('PORT', '5000')}"
+accesslog = "-"
+access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
+
+workers = int(os.getenv("GUNICORN_WORKERS", 1))
+threads = int(os.getenv("GUNICORN_WORKERS", 1))
+
+reload = bool("false")
+
+timeout = int(os.getenv("REQ_TIMEOUT", 300))
